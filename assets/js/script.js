4212d545c58241807345eb14396ce9bdc6453add
LFG115 <>
2023-11-04 11:10:11 +0000

    add correct and incorrect answer with a return message

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,7 +38,21 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Checks the answer against the first element in the 
+ * returned calculateCorrectAnswer array.
+ */
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert ("Hey you got it right! Well Done! ;D ");
+    } else {
+        alert (`Unlucky! you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    }
 
 }
 
@@ -58,7 +72,7 @@ function calculateCorrectAnswer() {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}. Aborting!`;
     }
-    
+
 }
 
 function incrementScore() {
